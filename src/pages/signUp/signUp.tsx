a8dc81722a1120adcc01f02c4609c0d13d8c7961
svyatoslavkk <sv.krivonos1@gmail.com>
2024-01-05 16:02:33 +0300

    feat: add sign up layout

--- src/pages/signUp/signUp.tsx
+++ src/pages/signUp/signUp.tsx
@@ -1,7 +1,14 @@
+import GoogleIcon from '@mui/icons-material/Google';
+
 export default function SignUp() {
   return (
-    <div>
-      
+    <div className="container">
+      <input className="classic-input" placeholder="Email" />
+      <input className="classic-input" placeholder="Password" />
+      <button>Sign Up</button>
+      <div className="socials">
+        <GoogleIcon />
+      </div>
     </div>
   )
 }
