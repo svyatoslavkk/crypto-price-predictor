81f7062d52f77106d4f77133cbfbc9fd919590d7
svyatoslavkk <sv.krivonos1@gmail.com>
2024-01-05 17:03:35 +0300

    feat: add minixs

--- src/pages/signUp/signUp.tsx
+++ src/pages/signUp/signUp.tsx
@@ -2,7 +2,7 @@ import GoogleIcon from '@mui/icons-material/Google';
 
 export default function SignUp() {
   return (
-    <div className="container">
+    <div className="signup">
       <input className="classic-input" placeholder="Email" />
       <input className="classic-input" placeholder="Password" />
       <button>Sign Up</button>
