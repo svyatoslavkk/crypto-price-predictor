a8dead9cfef61cbe7561029ebb15abf94205ff67
svyatoslavkk <sv.krivonos1@gmail.com>
2024-01-05 17:37:46 +0300

    feat: add sign up styles

--- src/pages/signUp/signUp.tsx
+++ src/pages/signUp/signUp.tsx
@@ -3,12 +3,39 @@ import GoogleIcon from '@mui/icons-material/Google';
 export default function SignUp() {
   return (
     <div className="signup">
-      <input className="classic-input" placeholder="Email" />
-      <input className="classic-input" placeholder="Password" />
-      <button>Sign Up</button>
-      <div className="socials">
-        <GoogleIcon />
+      <div className="ring">
+        <i style={{ '--clr' : '#00ff0a' } as React.CSSProperties}></i>
+        <i style={{ '--clr' : '#ff0057' } as React.CSSProperties}></i>
+        <i style={{ '--clr' : '#fffd44' } as React.CSSProperties}></i>
+        <div className="login">
+          <h2>Register</h2>
+          <div className="inputBx">
+            <input type="text" className="classic-input" placeholder="Username" />
+          </div>
+          <div className="inputBx">
+            <input type="text" className="classic-input" placeholder="Email" />
+          </div>
+          <div className="inputBx">
+            <input type="password" className="classic-input" placeholder="Password" />
+          </div>
+          <div className="inputBx">
+            <input type="submit" className="classic-input" value="Sign Up" />
+          </div>
+          <div className="links">
+            <div className="social-icon">
+              <GoogleIcon fontSize='large' />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+    // <div className="signup">
+    //   <input className="classic-input" placeholder="Email" />
+    //   <input className="classic-input" placeholder="Password" />
+    //   <button>Sign Up</button>
+    //   <div className="socials">
+    //     <GoogleIcon />
+    //   </div>
+    // </div>
   )
 }
