5932d05f9f61d0c0dd9ac1ccaf7df35f3433dec9
svyatoslavkk <sv.krivonos1@gmail.com>
2024-01-06 16:34:41 +0300

    feat: add price updating real time

--- src/components/topCoin/TopCoin.tsx
+++ src/components/topCoin/TopCoin.tsx
@@ -1,106 +1,144 @@
-// import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useGetBitcoinInfoQuery } from "../../redux/features/api/api";
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import TrendingDownIcon from '@mui/icons-material/TrendingDown';
-// import { formatData } from "../../utils/formatData";
+import { formatData } from "../../utils/formatData";
 
 export default function TopCoin() {
-  // const [currencies, setcurrencies] = useState([]);
-  // const [pair, setpair] = useState("");
-  // const [price, setprice] = useState("0.00");
-  // const [pastData, setpastData] = useState({});
-  // const ws = useRef(new WebSocket("wss://ws-feed.pro.coinbase.com"));
+  const [currencies, setcurrencies] = useState<any[]>([]);
+  const [pair, setpair] = useState("BTC-USD");
+  const [price, setprice] = useState("0.00");
+  const [pastData, setpastData] = useState({});
+  const [currentBitcoinPrice, setCurrentBitcoinPrice] = useState("");
+  const ws = useRef(new WebSocket("wss://ws-feed.pro.coinbase.com"));
 
-  // let first = useRef(false);
-  // const url = "https://api.pro.coinbase.com";
+  let first = useRef(false);
+  const url = "https://api.pro.coinbase.com";
 
-  // useEffect(() => {
-  //   let pairs: any[] = [];
+  useEffect(() => {
+    let pairs: any[] = [];
 
-  //   const apiCall = async () => {
-  //     await fetch(url + "/products")
-  //       .then((res) => res.json())
-  //       .then((data) => (pairs = data));
+    const apiCall = async () => {
+      await fetch(url + "/products")
+        .then((res) => res.json())
+        .then((data) => (pairs = data));
 
-  //     let filtered = pairs.filter((pair) => {
-  //       if (pair.quote_currency === "USD") {
-  //         return pair;
-  //       }
-  //     });
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === "USD") {
+          return pair;
+        }
+      });
 
-  //     filtered = filtered.sort((a, b) => {
-  //       if (a.base_currency < b.base_currency) {
-  //         return -1;
-  //       }
-  //       if (a.base_currency > b.base_currency) {
-  //         return 1;
-  //       }
-  //       return 0;
-  //     });
+      filtered = filtered.sort((a, b) => {
+        if (a.base_currency < b.base_currency) {
+          return -1;
+        }
+        if (a.base_currency > b.base_currency) {
+          return 1;
+        }
+        return 0;
+      });
 
-  //     console.log("filtered", filtered);
-  //     setcurrencies(filtered);
+      console.log("filtered", filtered);
+      setcurrencies(filtered);
 
-  //     first.current = true;
-  //   };
+      first.current = true;
+    };
 
-  //   apiCall();
-  // }, []);
+    apiCall();
+  }, []);
 
-  // useEffect(() => {
-  //   if (!first.current) {
-  //     return;
-  //   }
+  useEffect(() => {
+    if (!first.current) {
+      return;
+    }
 
-  //   let msg = {
-  //     type: "subscribe",
-  //     product_ids: [pair],
-  //     channels: ["ticker"],
-  //   };
-  //   let jsonMsg = JSON.stringify(msg);
-  //   if (ws.current) {
-  //     ws.current.send(jsonMsg);
-  //   }
+    let msg = {
+      type: "subscribe",
+      product_ids: [pair],
+      channels: ["ticker"],
+    };
+    let jsonMsg = JSON.stringify(msg);
+    if (ws.current) {
+      ws.current.send(jsonMsg);
+    }
 
-  //   let historicalDataURL = `${url}/products/${pair}/candles?granularity=86400`;
-  //   const fetchHistoricalData = async () => {
-  //     let dataArr: any[] = [];
-  //     await fetch(historicalDataURL)
-  //       .then((res) => res.json())
-  //       .then((data) => (dataArr = data));
+    let historicalDataURL = `${url}/products/${pair}/candles?granularity=86400`;
+    const fetchHistoricalData = async () => {
+      let dataArr: any[] = [];
+      await fetch(historicalDataURL)
+        .then((res) => res.json())
+        .then((data) => (dataArr = data));
 
-  //     let formattedData = formatData(dataArr);
-  //     setpastData(formattedData);
-  //   };
+      let formattedData = formatData(dataArr);
+      setpastData(formattedData);
+      console.log(pastData)
+    };
 
-  //   fetchHistoricalData();
+    fetchHistoricalData();
 
-  //   if (ws.current) {
-  //     ws.current.onmessage = (e: MessageEvent) => {
-  //       let data = JSON.parse(e.data);
-  //       if (data.type !== "ticker") {
-  //         return;
-  //       }
+    if (ws.current) {
+      ws.current.onmessage = (e: MessageEvent) => {
+        let data = JSON.parse(e.data);
+        if (data.type !== "ticker") {
+          return;
+        }
 
-  //       if (data.product_id === pair) {
-  //         setprice(data.price);
-  //       }
-  //     };
-  //   }
-  // }, [pair]);
+        if (data.product_id === pair) {
+          setprice(data.price);
+        }
 
-  // const handleSelect = (e) => {
-  //   let unsubMsg = {
-  //     type: "unsubscribe",
-  //     product_ids: [pair],
-  //     channels: ["ticker"]
-  //   };
-  //   let unsub = JSON.stringify(unsubMsg);
+        if (data.product_id === "BTC-USD") {
+          setCurrentBitcoinPrice(data.price);
+        }
+      };
+    }
+  }, [pair]);
 
-  //   ws.current.send(unsub);
+  useEffect(() => {
+    const fetchBitcoinPrice = async () => {
+      const response = await fetch(`${url}/products/BTC-USD/ticker`);
+      const bitcoinData = await response.json();
+      
+      setCurrentBitcoinPrice(bitcoinData.price);
+    };
 
-  //   setpair(e.target.value);
-  // };
+    fetchBitcoinPrice();
+
+    const bitcoinWs = new WebSocket("wss://ws-feed.pro.coinbase.com");
+    bitcoinWs.onopen = () => {
+      bitcoinWs.send(
+        JSON.stringify({
+          type: "subscribe",
+          product_ids: ["BTC-USD"],
+          channels: ["ticker"],
+        })
+      );
+    };
+    bitcoinWs.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      if (data.type === "ticker") {
+        setCurrentBitcoinPrice(data.price);
+      }
+    };
+
+    return () => {
+      bitcoinWs.close();
+    };
+  }, []);
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    let unsubMsg = {
+      type: "unsubscribe",
+      product_ids: [pair],
+      channels: ["ticker"]
+    };
+    let unsub = JSON.stringify(unsubMsg);
+
+    ws.current.send(unsub);
+
+    setpair(e.target.value);
+  };
 
   const { data: bitcoinInfo } = useGetBitcoinInfoQuery('bitcoin');
 
@@ -111,9 +149,9 @@ export default function TopCoin() {
   return (
     <div className="top-coin">
       <h2 className="medium-header">Top Coin</h2>
-      {/* <div>
+      <div>
         <select name="currency" value={pair} onChange={handleSelect}>
-          {currencies.map((cur, idx) => {
+          {currencies.map((cur: { id: string, display_name: string }, idx: number) => {
             return (
               <option key={idx} value={cur.id}>
                 {cur.display_name}
@@ -122,15 +160,16 @@ export default function TopCoin() {
           })}
         </select>
         <h2>PRICE{`$${price}`}</h2>
-      </div> */}
-      {bitcoinInfo && (
+      </div>
       <div className="window">
-        <div className="coin-main-info">
-          <img src={bitcoinInfo.image.small} className="small-circle-img" alt="Coin" />
-          <h3 className="small-header">{bitcoinInfo.name}</h3>
-        </div>
+        {bitcoinInfo && (
+          <div className="coin-main-info">
+            <img src={bitcoinInfo.image.small} className="small-circle-img" alt="Coin" />
+            <h3 className="small-header">{bitcoinInfo.name}</h3>
+          </div>
+        )}
         <div className="price-info">
-          <span>${bitcoinInfo.market_data.current_price.usd}</span>
+          <span>{`$${pair === "BTC-USD" ? currentBitcoinPrice : price}`}</span>
         </div>
         <div className="buttons">
           <button className="up-btn">
@@ -143,7 +182,6 @@ export default function TopCoin() {
           </button>
         </div>
       </div>
-      )}
     </div>
   )
 }
